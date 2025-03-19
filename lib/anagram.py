ab508c97aa30b1422e173df8a5f405f3a4e773e6
KairuMaina <george.kairu@student.moringaschool.com>
2025-03-20 01:29:24 +0300

    completed anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_letters = sorted(self.word)
+
+    def match(self, candidates):
+        return [word for word in candidates if word.lower() != self.word and sorted(word.lower()) == self.sorted_letters]
